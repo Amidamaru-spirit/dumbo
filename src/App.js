051e05a2f8bb6e6ea441ef56e883c9f58b043554
Your Name <mahaev99@gmail.com>
2023-10-18 10:29:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [openHead, setOpenHead] = React.useState(false);
+	const [openFoot, setOpenFoot] = React.useState(false);
+	React.useEffect(() => {
+		setTimeout(() => {
+			setOpenHead(true);
+		}, 1000);
+		setTimeout(() => {
+			setOpenFoot(true);
+		}, 2000);
+	}, []);
+	return (
+		<div className="App">
+			<div className={openHead ? 'head__open' : 'head__close'}>
+				<h1 className="head__head">Ищешь ответы?</h1>
+			</div>
+			<div className={openFoot ? 'foot__open' : 'foot__close'}>
+				<h2 className="foot__head">Просишь помощи у коллег?</h2>
+			</div>
+			<div className="container">
+				<a href="https://www.google.com/?hl=RU">Узнать ответ</a>
+			</div>
+		</div>
+	);
 }
 
 export default App;
